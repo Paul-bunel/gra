f6c8279c8c73cd0cbcce9da6c1da81b1b85058f0
Paul-bunel <paulbunel34@gmail.com>
2022-05-13 15:59:12 +0200

    Update source and profile : protein -> nucleotide, new genes : TAS1R[123]

---
+++ make_profile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SRC=$1
+
+name=$(sed 's|.*/||' <<< $SRC | sed 's/msafile_//' | sed 's/\..*//')
+printf "File : $name\n"
+gene=$(grep -o 'TAS[12]R[123]*' <<< $name)
+printf "Gene : $gene\n\n"
+
+TAS=''
+[[ $name == *TAS* ]] && TAS='/TAS'
+
+[ ! -d "HMM_PROFILE$TAS/${gene^^}" ] && mkdir "HMM_PROFILE$TAS/${gene^^}" &&
+    printf "Creating directory 'HMM_PROFILE$TAS/${gene^^}'\n\n"
+
+printf "Building HMM profile...\n"
+printf "hmmbuild -o \"/dev/null\" --dna \"HMM_PROFILE$TAS/${gene^^}/$name.hmm\" $SRC\n\n"
+hmmbuild -o "/dev/null" --dna "HMM_PROFILE$TAS/${gene^^}/$name.hmm" $SRC
+
+printf "Converting to binaries files\n"
+printf "hmmpress -f \"HMM_PROFILE$TAS/${gene^^}/$name.hmm\" > /dev/null\n"
+hmmpress -f "HMM_PROFILE$TAS/${gene^^}/$name.hmm" > /dev/null
--- stats_tblout.py
+++ stats_tblout.py
@@ -4,6 +4,7 @@ from statistics import *
 import json
 
 def stats(dbpath):
+    print("dbpath =", dbpath)
     total_results = []
     dir_res = {}
     for file in os.listdir(dbpath):
@@ -23,28 +24,54 @@ def stats(dbpath):
     dir_res["general_mean"] = mean([float(row[4]) for row in total_results])
     return dir_res
 
+def stat(filename):
+    results = []
+    dir_res = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            if line[0] != '#':
+                results.append(line.split())
+    dir_res[filename] = [
+        mean([float(row[4]) for row in results]),
+        min([float(row[4]) for row in results]),
+        max([float(row[4]) for row in results])
+    ]
+
+    return dir_res
+
 
 def main():
     HMMstats = {}
     if len(sys.argv) == 1:
+        stat_filename = "stats.json"
         databases = os.listdir("RESULTS")
+    elif len(sys.argv) == 2:
+        HMMstats = stat(sys.argv[1])
+        print(f"HMM_PROFILE\t{'DB SCANNED':<30}   {'MEAN':<17}{'MIN':<10}{'MAX':<10}")
+        print("_____________________________________________________________________________________\n")
+
+        [print(f"{key :<30}: {value}") for key, value in HMMstats.items()]
     else:
         databases = sys.argv[1:]
+        stat_filename = ""
+        for db in databases:
+            stat_filename += db + '_'
+        stat_filename = stat_filename[:-1] + ".json"
 
-    for db in databases:
-        dbpath = os.path.join("RESULTS", db)
-        HMMstats[db] = stats(dbpath)
+    # for db in databases:
+    #     dbpath = os.path.join("RESULTS", db)
+    #     HMMstats[db] = stats(dbpath)
 
-    print(f"HMM_PROFILE\t{'DB SCANNED':<30}   {'MEAN':<17}{'MIN':<10}{'MAX':<10}")
-    print("_____________________________________________________________________________________\n")
+    # print(f"HMM_PROFILE\t{'DB SCANNED':<30}   {'MEAN':<17}{'MIN':<10}{'MAX':<10}")
+    # print("_____________________________________________________________________________________\n")
 
-    for k in HMMstats.keys():
-        print(k, ':')
-        [print(f"\t{key :<30}: {value}") for key, value in HMMstats[k].items()]
+    # for k in HMMstats.keys():
+    #     print(k, ':')
+    #     [print(f"\t{key :<30}: {value}") for key, value in HMMstats[k].items()]
 
 
-    json_str = json.dumps(HMMstats, skipkeys = False, indent = 4)
-    with open("stats.json", 'w') as file:
-        file.write(json_str)
+    # json_str = json.dumps(HMMstats, skipkeys = False, indent = 4)
+    # with open(stat_filename, 'w') as file:
+    #     file.write(json_str)
         
 main()
