46765cdfef7c0792f2e2f881098b3271e76681de
Paul-bunel <paulbunel34@gmail.com>
2022-05-19 13:35:32 +0200

    Ajout de la lecture de FASTA et récupération du résultat du HMM_Scan

--- gra.c
+++ gra.c
@@ -58,6 +58,39 @@ struct String getFiles(int n, char **filenames) {
     return res;
 }
 
+FILE* hmmscan(char *profile, struct String fasta) {
+    /**
+        hmmscan -o "/dev/null" \
+        --tblout "RESULTS/${name^^}/res_$DBname.txt" \
+        --qformat $format \
+        "HMM_PROFILE/${name^^}/$name.hmm" \
+        $db
+     */
+    char *fun = "/bin/bash -c 'hmmscan -o /dev/null --tblout /dev/stdout --qformat fasta ";
+    size_t l_arg = strlen(profile) + strlen(" /dev/stdin <<< \"\"'") + fasta.len;
+    char *arg = calloc(l_arg, sizeof(char));
+    snprintf(arg, l_arg, "%s /dev/stdin <<< \"%s\"'", profile, fasta.str);
+
+    size_t l_cmd = strlen(fun) + l_arg;
+    char *cmd = calloc(l_cmd, sizeof(char));
+    strcpy(cmd, fun);
+    strcat(cmd, arg);
+
+    FILE *scan_res = popen(cmd, "r");
+    if (scan_res == NULL) {
+        fprintf(stderr, "Erreur lors du scan HMM");
+    }
+
+    return scan_res;
+}
+
+void parse_scan_res(FILE *scan_res) {
+    char buffer[512];
+    while (fgets(buffer, sizeof(buffer), scan_res) != NULL) {
+        if (buffer[0] != '#') { printf("%s", buffer); }
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         fprintf(stderr, "Utilisation : %s fichier1.fasta fichier2.fasta ...\n", argv[0]);
@@ -66,18 +99,25 @@ int main(int argc, char **argv) {
 
     struct String fasta = getFiles(argc - 1, &argv[1]);
 
-    printf("Réalisation de la commande bash...\n");
-    int l_cmd = strlen("/bin/bash -c 'head <<< \"") + fasta.len + 3;
-    char *cmd = calloc(l_cmd, sizeof(char));
-    strcpy(cmd, "/bin/bash -c 'head <<< \"");
-    strcat(cmd, fasta.str);
+    FILE *scan_res = hmmscan("HMM_PROFILE/TAS/TAS_ncbi_nuc.hmm", fasta);
+    parse_scan_res(scan_res);
 
-    cmd[l_cmd - 3] = '\'';
-    cmd[l_cmd - 2] = '"';
-    cmd[l_cmd - 1] = '\0';
+    // printf("Réalisation de la commande bash...\n");
+    // int l_cmd = strlen("/bin/bash -c 'head <<< \"") + fasta.len + 2;
+    // char *cmd = calloc(l_cmd, sizeof(char));
+    // strcpy(cmd, "/bin/bash -c 'head <<< \"");
+    // strcat(cmd, fasta.str);
 
-    printf("Commande : \n");
-    int status = system(cmd);
+    // cmd[l_cmd - 3] = '"';
+    // cmd[l_cmd - 2] = '\'';
+    // cmd[l_cmd - 1] = '\0';
+
+    // printf("Commande : \n");
+
+    // for (int i = 10; i >= 1; i--) {
+    //     printf("cmd[l_cmd - %d] = %i\n", i, cmd[l_cmd - i]);
+    // }
+    // int status = system(cmd);
 
     return EXIT_SUCCESS;
 }
